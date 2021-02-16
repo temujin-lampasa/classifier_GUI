77a7772eb3597325b0a0dbcb4c1bee07a9d16c54
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2021-02-16 16:16:21 +0800

    Add clear button

--- classifier_gui.py
+++ classifier_gui.py
@@ -1,11 +1,16 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from mainwindow import Ui_MainWindow
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.clear_btn.clicked.connect(self.clear_drawing)
+    
+    def clear_drawing(self):
+        self.drawing.pixmap().fill(QtGui.QColor('white'))
+        self.drawing.update()
 
 app = QtWidgets.QApplication(sys.argv)
 
